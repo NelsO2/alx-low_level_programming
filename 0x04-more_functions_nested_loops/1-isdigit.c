36e12624df6c6c1f673c3640a22bc644a8490cfc
NelsO2 <nelsonchampion19@gmail.com>
2022-09-18 12:00:47 -0700

    my 1-isupper.c code

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - a function that checks for a digit 0-9
- * @c: input
- * Return: 1 if c is digit, 0 otherwise
- */
+*_isupper - a function that checks for a digit 0-9
+*@c: input
+*Return: 1 if c is digit, 0 otherwise
+*/
+
 int _isupper(int c)
 {
-if ((c >= 48) && (c <= 57))
-return (1);
-else
-return (0);
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	
+	return (0);
 }
