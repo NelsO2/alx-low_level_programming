397eebeca41e791e0acf5d9f2e7b9a69c7adbd7e
NelsO2 <nelsonchampion19@gmail.com>
2022-09-22 05:10:54 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 
 /**
  *_strcat - concatenates the string pointed to by @src to
- *the end of the string pointed to by @dest
+ * the end of the string pointed to by @dest
  *@dest: String that will be appended
- *src: String to be concatenated
+ *@src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
  */
